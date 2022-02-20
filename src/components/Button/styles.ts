cc1ed90e87d43e201d72765fc5c6f77a395f6bc4
Bruno Farias <bruno_fariasd@hotmail.com>
2022-02-20 20:30:18 -0300

    refactor

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -6,7 +6,6 @@ export const Container = styled(Pressable)`
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
-  margin-bottom: 48px;
 
   align-items: center;
   justify-content: center;
--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -3,13 +3,12 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: #fff;
-  padding-horizontal: 20px;
+  padding: 40px 20px;
   align-items: center;
   justify-content: space-between;
 `;
 
 export const TitleViewText = styled.Text`
-  margin-top: 40px;
   font-size: 24px;
   font-family: RobotoSlab-Medium;
   color: #000000;
